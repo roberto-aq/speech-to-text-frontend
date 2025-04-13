7b76b112d378c3df1556dd6c2f6daa04c2ad7148
roberto-aq <baxro2000@gmail.com>
2025-04-13 10:55:22 -0500

    Añadidas ultimas modificaciones, loaders

--- src/app/dashboard/audio/page.tsx
+++ src/app/dashboard/audio/page.tsx
@@ -49,39 +49,59 @@ export default function DashboardAudioPage() {
 	};
 
 	return (
-		<Card className='max-w-lg mx-auto mt-10 p-6 shadow-lg rounded-lg'>
-			<h1 className='text-2xl font-bold text-center mb-4'>
-				🎙️ Subir Audio para Transcripción
-			</h1>
+		<>
+			<Card className='max-w-lg mx-auto mt-10 p-6 shadow-lg rounded-lg'>
+				<h1 className='text-2xl font-bold text-center mb-4'>
+					🎙️ Subir Audio para Transcripción
+				</h1>
 
-			{/* Input para seleccionar el archivo */}
-			<div className='flex flex-col items-center'>
-				<Input
-					type='file'
-					accept='audio/*'
-					onChange={handleFileChange}
-					className='mb-4'
-				/>
-				{file && <p className='text-sm text-gray-600'>{file.name}</p>}
-			</div>
+				{/* Input para seleccionar el archivo */}
+				<div className='flex flex-col items-center'>
+					<Input
+						type='file'
+						accept='audio/*'
+						onChange={handleFileChange}
+						className='mb-4'
+					/>
+					{file && (
+						<p className='text-sm text-gray-600'>{file.name}</p>
+					)}
+				</div>
 
-			{/* Botón de subida */}
-			<Button
-				onClick={handleUpload}
-				disabled={isPending || !file}
-				className='w-full mt-4 flex items-center justify-center'
-			>
-				{isPending ? (
-					<>
-						<Loader2 className='h-5 w-5 animate-spin mr-2' />{' '}
-						Subiendo...
-					</>
-				) : (
-					<>
-						<UploadCloud className='h-5 w-5 mr-2' /> Subir Audio
-					</>
-				)}
-			</Button>
-		</Card>
+				{/* Botón de subida */}
+				<Button
+					onClick={handleUpload}
+					disabled={isPending || !file}
+					className='w-full mt-4 flex items-center justify-center'
+				>
+					{isPending ? (
+						<>
+							<Loader2 className='h-5 w-5 animate-spin mr-2' />{' '}
+							Subiendo...
+						</>
+					) : (
+						<>
+							<UploadCloud className='h-5 w-5 mr-2' /> Subir Audio
+						</>
+					)}
+				</Button>
+
+				{/* Loader distintivo debajo del botón, solo se muestra cuando isPending es true */}
+			</Card>
+
+			{isPending && (
+				<div className='grid place-items-center mt-10'>
+					<div className='flex flex-col gap-3 items-center'>
+						<Loader2 className='size-14 animate-spin text-violet-300' />
+						<span className='text-lg  text-violet-300 flex flex-col space-y-1 items-center'>
+							<span className='font-semibold'>Procesando...</span>
+							<span className='font-medium text-indigo-50 text-base'>
+								Esto puede tomar unos minutos.
+							</span>
+						</span>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
